5555c832002975762b9df4edc5dcb096f437d1bb
dheysonlee99 <dheysonlee@hotmail.com>
2016-01-06 11:47:20 -0200

    java

--- TestaFuncionario.java
+++ TestaFuncionario.java
@@ -1,11 +1,12 @@
 class TestaFuncionario{
 	public static void main(String[] args) {
 		Funcionario Rm = new Funcionario();
+		Data data = new Data();
 
+		
 		Rm.nome = "Dheyson lee";
 		Rm.salario = 1000;
 		Rm.departamento = "limpeza"
-		Rm.data = "30/10/1994"
 		Rm.rg = "123-09"
 		Rm.recebeAumento(900);
 
@@ -18,7 +19,6 @@ class TestaFuncionario{
 		Bn.nome = "Joao da Barra";
 		Bn.salario = 10000;
 		Bn.departamento = "presidente"
-		Bn.data = "10/10/2010"
 		Bn.rg = "123-51"
 		Bn.recebeAumento(9000);
 
@@ -30,12 +30,30 @@ class TestaFuncionario{
 		Jn.nome = "Ricardo Constancio";
 		Jn.salario = 23000;
 		Jn.departamento = "gari"
-		Jn.data = "30/12/1998"
 		Jn.rg = "510-88"
 		Jn.recebeAumento(9600);
 
 		System.out.println("salario atual: " + Jn.salario);
 		System.out.println("ganho anual: " + Jn.calculoGanhoAnual);
+		
+		Funcionario Fj = new Funcionario();
 
+		Fj.nome = "Maria dos Anjos"
+		Fj.salario = 30000
+		Fj.departamento = "servicos gerais"
+		Fj.rg = "8893-51"
+
+		Funcionario Fk = new Funcionario();
+
+		Fk.nome = "Maria dos Anjos"
+		Fk.salario = 30000
+		Fk.departamento = "servicos gerais"
+		Fk.rg = "8893-51"
+
+		if(Fj == Fk){
+			System.out.println("iguais");
+		}else{
+			System.out.println("diferentes");
+		}
 	}
 }
