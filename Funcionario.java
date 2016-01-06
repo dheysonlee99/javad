c28be0f8bac66a74167046a843c5532ab11cd48d
dheysonlee99 <dheysonlee@hotmail.com>
2016-01-06 11:50:15 -0200

    java

--- Funcionario.java
+++ Funcionario.java
@@ -1,8 +1,8 @@
 class Funcionario{
+	data dataDeEntrada;
 	String nome;
 	String departamento;
 	float salario;
-	String data;
 	String rg;
 
 	void recebeAumento(float aumento){
@@ -16,7 +16,10 @@ class Funcionario{
 		System.out.println("Nome: " + this.nome);
 		System.out.println("Nome: " + this.departamento);
 		System.out.println("Nome: " + this.salario);
-		System.out.println("Nome: " + this.data);
+		System.out.println("Nome: " + this.dataDeEntrada);
 		System.out.println("Nome: " + this.rg);
+		System.out.println("Nome: " + this.dataDeEntrada.dia);
+		System.out.println("Nome: " + this.dataDeEntrada.mes);
+		System.out.println("Nome: " + this.dataDeEntrada.ano);	
 	}
 }
